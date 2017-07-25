a159594e08c0580893007ed0cff1837ae459277c
cmonteleoni <30416987+cmonteleoni@users.noreply.github.com>
2017-07-25 14:08:37 -0400

    Add files via upload

---
+++ research_files/research.js
@@ -0,0 +1,9 @@
+// Created by iWeb 3.0.4 local-build-20170217
+
+setTransparentGifURL('Media/transparent.gif');function hostedOnDM()
+{return false;}
+function onPageLoad()
+{loadMozillaCSS('research_files/researchMoz.css')
+adjustLineHeightIfTooBig('id1');adjustFontSizeIfTooBig('id1');adjustLineHeightIfTooBig('id2');adjustFontSizeIfTooBig('id2');Widget.onload();fixupAllIEPNGBGs();fixAllIEPNGs('Media/transparent.gif');performPostEffectsFixups()}
+function onPageUnload()
+{Widget.onunload();}
