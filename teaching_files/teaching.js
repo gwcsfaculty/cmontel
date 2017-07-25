fec97a54c28c2c7d82e85d504d20c2edec95001f
cmonteleoni <30416987+cmonteleoni@users.noreply.github.com>
2017-07-25 13:56:24 -0400

    Add files via upload

---
+++ teaching_files/teaching.js
@@ -0,0 +1,9 @@
+// Created by iWeb 3.0.4 local-build-20170123
+
+setTransparentGifURL('Media/transparent.gif');function hostedOnDM()
+{return false;}
+function onPageLoad()
+{loadMozillaCSS('teaching_files/teachingMoz.css')
+adjustLineHeightIfTooBig('id1');adjustFontSizeIfTooBig('id1');adjustLineHeightIfTooBig('id2');adjustFontSizeIfTooBig('id2');Widget.onload();fixupAllIEPNGBGs();fixAllIEPNGs('Media/transparent.gif');performPostEffectsFixups()}
+function onPageUnload()
+{Widget.onunload();}
