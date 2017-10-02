bea10d79974bf2360104611d371920b1e3659332
cmonteleoni <30416987+cmonteleoni@users.noreply.github.com>
2017-10-02 12:56:01 -0400

    Add files via upload
    Webpage update Oct 2017

--- climateInformatics_files/climateInformatics.js
+++ climateInformatics_files/climateInformatics.js
@@ -1,9 +1,9 @@
-// Created by iWeb 3.0.4 local-build-20170125
+// Created by iWeb 3.0.4 local-build-20171002
 
 setTransparentGifURL('Media/transparent.gif');function hostedOnDM()
 {return false;}
 function onPageLoad()
 {loadMozillaCSS('climateInformatics_files/climateInformaticsMoz.css')
-adjustLineHeightIfTooBig('id1');adjustFontSizeIfTooBig('id1');adjustLineHeightIfTooBig('id2');adjustFontSizeIfTooBig('id2');Widget.onload();fixupAllIEPNGBGs();fixAllIEPNGs('Media/transparent.gif');performPostEffectsFixups()}
+adjustLineHeightIfTooBig('id1');adjustFontSizeIfTooBig('id1');adjustLineHeightIfTooBig('id2');adjustFontSizeIfTooBig('id2');Widget.onload();fixAllIEPNGs('Media/transparent.gif');performPostEffectsFixups()}
 function onPageUnload()
 {Widget.onunload();}
